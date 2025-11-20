be045a60fdd926e8730d36a349582d62778f6a0b
yuliya <gyuliya2001@gmail.com>
2025-11-20 21:39:06 +0300

    [feat] add create seller profile via comment logic

---
+++ src/main/java/by/ratingsystem/dto/CommentAndSellerCreateDto.java
@@ -0,0 +1,27 @@
+package by.ratingsystem.dto;
+
+public class CommentAndSellerCreateDto {
+    private CommentCreateDto commentDto;
+    private SellerProfileCreateDto sellerDto;
+
+    public CommentAndSellerCreateDto(CommentCreateDto commentDto, SellerProfileCreateDto sellerDto) {
+        this.commentDto = commentDto;
+        this.sellerDto = sellerDto;
+    }
+
+    public CommentCreateDto getCommentDto() {
+        return commentDto;
+    }
+
+    public void setCommentDto(CommentCreateDto commentDto) {
+        this.commentDto = commentDto;
+    }
+
+    public SellerProfileCreateDto getSellerDto() {
+        return sellerDto;
+    }
+
+    public void setSellerDto(SellerProfileCreateDto sellerDto) {
+        this.sellerDto = sellerDto;
+    }
+}
