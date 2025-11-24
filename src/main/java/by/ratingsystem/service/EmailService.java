7473c3134c14f892e9eb04b6036d39f62e3b4161
yuliya <gyuliya2001@gmail.com>
2025-11-24 18:32:23 +0300

    [refactor] cleanup email service

--- src/main/java/by/ratingsystem/service/EmailService.java
+++ src/main/java/by/ratingsystem/service/EmailService.java
@@ -9,15 +9,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+    private final JavaMailSender emailSender;
+
     @Autowired
-    private JavaMailSender emailSender;
+    public EmailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
 
     public void sendVerificationEmail(String to, String subject, String text) throws MessagingException {
-        MimeMessage message = this.emailSender.createMimeMessage();
+        MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(text, true);
-        this.emailSender.send(message);
+        emailSender.send(message);
     }
 }
